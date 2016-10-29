739eef0a0a6c8bae3f3a1e8084f33064bf49c8e7
mazakk94 <mazakk94@gmail.com>
2016-10-29 16:09:06 -0500

    modal works

--- MvvmLight5/Overview.xaml.cs
+++ MvvmLight5/Overview.xaml.cs
@@ -12,6 +12,12 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
+
+using GalaSoft.MvvmLight.Ioc;
+using GalaSoft.MvvmLight.Messaging;
+using MvvmLight5.Helpers;
+using MvvmLight5.ViewModel;
+
 namespace MvvmLight5
 {
     /// <summary>
@@ -22,8 +28,36 @@ namespace MvvmLight5
         public Overview()
         {
             InitializeComponent();
-
+            Closing += (s, e) => ViewModelLocator.Cleanup();
+            Messenger.Default.Register<OpenWindowMessage>(
+              this,
+              message =>
+              {
+                  if (message.Type == WindowType.kModal)
+                  {
+                      var modalWindowVM = SimpleIoc.Default.GetInstance<ModalWindowViewModel>();
+                      modalWindowVM.MyText = message.Argument;
+                      var modalWindow = new ModalWindow()
+                      {
+                          DataContext = modalWindowVM
+                      };
+                      var result = modalWindow.ShowDialog() ?? false;
+                      Messenger.Default.Send(result ? "Accepted" : "Rejected");
+                  }
+                  else
+                  {
+                      var uniqueKey = System.Guid.NewGuid().ToString();
+                      var nonModalWindowVM = SimpleIoc.Default.GetInstance<NonModalWindowViewModel>(uniqueKey);
+                      nonModalWindowVM.MyText = message.Argument;
+                      var nonModalWindow = new NonModalWindow()
+                      {
+                          DataContext = nonModalWindowVM
+                      };
+                      nonModalWindow.Closed += (sender, args) => SimpleIoc.Default.Unregister(uniqueKey);
+                      nonModalWindow.Show();
+                  }
+              });
         }
     }
-}
 
+}
