f53a7b1545fca6711220f63645f55ebce6bfa753
mazakk94 <mazakk94@gmail.com>
2016-11-12 23:37:24 -0600

    timer works !

--- UserInterface/ViewModel/SolveTestViewModel.cs
+++ UserInterface/ViewModel/SolveTestViewModel.cs
@@ -5,6 +5,7 @@ using Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Windows.Threading;
 
 namespace UserInterface.ViewModel
 {
@@ -29,6 +30,50 @@ namespace UserInterface.ViewModel
             }
         }
 
+        private string _content;
+        public string Content
+        {
+            get
+            {
+                return _content;
+            }
+            set
+            {
+                _content = value;
+                RaisePropertyChanged(() => Content);
+            }
+        }
+
+        private List<string> _answer;
+        public List<string> Answer
+        {
+            get
+            {
+                return _answer;
+            }
+            set
+            {
+                _answer = value;
+                RaisePropertyChanged(() => Answer);
+            }
+        }
+
+        private TimeSpan _timeleft;
+        public TimeSpan Timeleft
+        {
+            get
+            {
+                return _timeleft;
+            }
+            set
+            {
+                _timeleft = value;
+                RaisePropertyChanged(() => Timeleft);
+            }
+        }
+
+        public DispatcherTimer Timer;
+
         #endregion
 
         public SolveTestViewModel(IDataService dataService)
@@ -37,23 +82,43 @@ namespace UserInterface.ViewModel
 
             _dataService = dataService;
             _dataService.GetData((item, error) => { if (error != null) return; });
-            
-            _testName = "";
-            TestName = "";
-
+                       
             #endregion
 
-            ClearWindow();
-        } 
-       
+            
+            
+            //ClearWindow();
+        }
+
+        private void DispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            Timeleft = Timeleft - new TimeSpan(0, 0, 1);
+            if (Timeleft == new TimeSpan(0,0,0))
+            {
+                Timer.Stop();
+                Content = "TIME OVER";
+                //here ends the test and it now has to count points for test, create and save history of test
+            }
+        }
+
         
         #region methods
 
         public void ClearWindow()
         {
-            //_testName = "";
             TestName = "Test Name";
-        }        
+            Answer = new List<string>();
+            for(var i = 0; i < 5; i++) Answer.Add(i.ToString());
+            Content = "Question content?";
+                        
+            Timer = new DispatcherTimer();
+            Timer.Tick += DispatcherTimer_Tick;
+            Timer.Interval = new TimeSpan(0, 0, 1);
+            Timer.Start();
+            Timeleft = new TimeSpan(0, 0, 10);
+                     
+
+        }
 
         internal void RefreshDAO()
         {
